1f0b386ca482765786d0fe2de2ae98322dde407d
Freddie J. Heather <30920629+FreddieJH@users.noreply.github.com>
2023-03-30 16:53:46 +1100

    all items

---
+++ empty_script.R
@@ -0,0 +1,19 @@
+# Author:
+# Date:
+# Code description:
+
+
+# Basic commands ---------------------------------------------------------------
+
+# Assign a numeric variable (any number you like) to an object (name the object num1)
+# Name a character string variable (any word you like) to an object (name the object string1), note the character string must be wrapped in quotation marks (otherwise R thinks it is an object)
+# add 3 to num1
+# multiple the first 10 even numbers by the first 10 odd numbers
+
+
+# Lets produce some errors/warnings --------------------------------------------
+# add 3 to stringr1 (error in object class)
+# type string2 in the console (unknown object class)
+# Add NA to your numeric variable (including NA values error)
+
+
